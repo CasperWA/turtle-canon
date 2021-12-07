5843d912aa85932f3517256313e098550d61a4f1
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2021-12-07 11:58:19 +0100

    Add tests for checking RDFlib consistency
    Keep canonized editions from using different RDFlib versions on the same
    input file and compare the content in a test.
    This ensures that if there are any inconsistencies between the RDFlib
    versions used, they should be picked up on - note, the weak link here is
    the base input file. If it does not contain enough edge cases, this is
    not trustworthy.

--- tests/test_turtle_canon.py
+++ tests/test_turtle_canon.py
@@ -2,6 +2,8 @@
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+import pytest
+
 if TYPE_CHECKING:
     from typing import List
 
@@ -30,10 +32,9 @@ def test_repetitivity(simple_turtle_file: Path) -> None:
         for j in range(i - 1):
             canonize(simple_turtle_file.with_stem(f"{simple_turtle_file.stem}_{j}"))
             for turtle_file_copy in listdir(tmp_dir):
-                assert (
-                    simple_turtle_file_content
-                    == (Path(tmp_dir) / turtle_file_copy).read_text()
-                )
+                assert simple_turtle_file_content == (
+                    Path(tmp_dir) / turtle_file_copy
+                ).read_text(encoding="utf8")
 
 
 def test_permutated_files(single_turtle_permutations: "List[Path]") -> None:
@@ -48,4 +49,49 @@ def test_permutated_files(single_turtle_permutations: "List[Path]") -> None:
         for other_turtle_file in single_turtle_permutations:
             if other_turtle_file == turtle_file:
                 continue
-            assert turtle_file.read_text() == other_turtle_file.read_text()
+            assert turtle_file.read_text(
+                encoding="utf8"
+            ) == other_turtle_file.read_text(encoding="utf8")
+
+
+def test_rdflib_consistency(top_dir: Path) -> None:
+    """Test canonized versions of the `turtle_canon_tests.ttl` file with supported
+    RDFlib versions."""
+    import re
+
+    version_from_file_regex = r".*_(?P<version>[0-9]+(\.[0-9]+){2})\.ttl$"
+    all_rdflib_turtle_files = list(
+        (top_dir / "tests" / "static" / "rdflib_canonized").glob("*.ttl")
+    )
+    if len(all_rdflib_turtle_files) <= 1:
+        pytest.skip(
+            "Not enough supported RDFlib versions to compare canonization consistency."
+        )
+
+    for turtle_file in all_rdflib_turtle_files:
+        version_match = re.match(version_from_file_regex, turtle_file.name)
+        if version_match:
+            rdflib_version = version_match.group("version")
+        else:
+            pytest.fail(
+                f"Couldn't determine the RDFlib version from the file {turtle_file} !"
+            )
+
+        for other_turtle_file in all_rdflib_turtle_files:
+            if other_turtle_file == turtle_file:
+                continue
+            version_match = re.match(version_from_file_regex, other_turtle_file.name)
+            if version_match:
+                other_rdflib_version = version_match.group("version")
+            else:
+                pytest.fail(
+                    "Couldn't determine the RDFlib version from the file "
+                    f"{other_turtle_file} !"
+                )
+
+            assert turtle_file.read_text(
+                encoding="utf8"
+            ) == other_turtle_file.read_text(encoding="utf8"), (
+                f"The canonized files when using RDFlib versions {rdflib_version} and "
+                f"{other_rdflib_version} are not the same !"
+            )
