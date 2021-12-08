53eb6e47870bf34203f7b7cd1652609a340156ce
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2021-12-08 15:57:50 +0100

    Move `exit_after` to print message utility function

--- turtle_canon/cli/utils.py
+++ turtle_canon/cli/utils.py
@@ -7,7 +7,10 @@ if TYPE_CHECKING:  # pragma: no cover
 
 
 def _print_message(
-    message: str, target: "Optional[TextIO]" = None, prefix: "Optional[str]" = None
+    message: str,
+    target: "Optional[TextIO]" = None,
+    prefix: "Optional[str]" = None,
+    exit_after: bool = False,
 ) -> None:
     """Print a message to target.
 
@@ -15,12 +18,15 @@ def _print_message(
         message: The message to print.
         target: A file stream, defaults to STDOUT.
         prefix: String to prepend to message.
+        exit_after: Whether or not to call `sys.exit(1)` after printing the message.
 
     """
     target = target if target is not None else sys.stdout
     if prefix:
         message = prefix + message
     print(message, file=target, flush=True)
+    if exit_after:
+        sys.exit(1)
 
 
 def print_error(message: "Union[str, Exception]", exit_after: bool = True) -> None:
@@ -34,9 +40,7 @@ def print_error(message: "Union[str, Exception]", exit_after: bool = True) -> No
     res = f"Misfire !\n\n{message}"
     if isinstance(message, Exception):
         res += f"\n\nGeneral information about the exception: {message.__doc__}"
-    _print_message(res, target=sys.stderr, prefix="ERROR: ")
-    if exit_after:
-        sys.exit(1)
+    _print_message(res, target=sys.stderr, prefix="ERROR: ", exit_after=exit_after)
 
 
 def print_warning(message: "Union[str, Exception]", exit_after: bool = False) -> None:
@@ -50,6 +54,4 @@ def print_warning(message: "Union[str, Exception]", exit_after: bool = False) ->
     res = f"Don't come too close !\n\n{message}"
     if isinstance(message, Exception):
         res += f"\n\nGeneral information about the warning: {message.__doc__}"
-    _print_message(res, target=sys.stderr, prefix="WARNING: ")
-    if exit_after:
-        sys.exit(1)
+    _print_message(res, target=sys.stderr, prefix="WARNING: ", exit_after=exit_after)
